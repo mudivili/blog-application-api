05a44047a22cf20d7109a634432429381b3b8b62
Tamil Vendhan Kanagarasu <tamilvendhan.k@yahoo.com>
2021-09-05 20:26:21 +0530

    Formatted code

--- database/index.js
+++ database/index.js
@@ -4,38 +4,38 @@ const DatabaseInterface = require('./interface');
 
 class DatabaseBridge {
 
-	constructor(options, formatError) {
+  constructor(options, formatError) {
 
-		const uri = DatabaseBridge.buildURI(options);
+    const uri = DatabaseBridge.buildURI(options);
 
-		this.client = new MongoClient(uri);
-		this.database = null;
-		this.databaseName = options.name;
-		this.formatError = formatError;
-		this.interface = null;
+    this.client = new MongoClient(uri);
+    this.database = null;
+    this.databaseName = options.name;
+    this.formatError = formatError;
+    this.interface = null;
 
-	}
+  }
 
-	async initialize() {
+  async initialize() {
 
-		await this.client.connect();
+    await this.client.connect();
 
     this.database = this.client.db(this.databaseName);
     this.interface = new DatabaseInterface(this.database, this.formatError);
 
-		console.log('Database connected!');
+    console.log('Database connected!');
 
-	}
+  }
 
-	static buildURI(options) {
+  static buildURI(options) {
 
-		if(options.srv === true) {
-			return `mongodb+srv://${options.username}:${options.password}@${options.host}/?retryWrites=true&w=majority`;
-		}
+    if (options.srv === true) {
+      return `mongodb+srv://${options.username}:${options.password}@${options.host}/?retryWrites=true&w=majority`;
+    }
 
-		return `mongodb://${options.username}:${options.password}@${options.host}:${options.port}/?retryWrites=true&w=majority`;
+    return `mongodb://${options.username}:${options.password}@${options.host}:${options.port}/?retryWrites=true&w=majority`;
 
-	}
+  }
 
 }
 
